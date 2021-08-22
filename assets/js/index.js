cf545902ec0be2a50254734ec134dd214234f596
gervyg <gervygonzalez@gmail.com>
2021-08-22 13:00:30 -0400

    desafio4.2-js-paletacolores

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+//Cambio de colores
+//Cambio de Color 1
+document.getElementById("btn-1").addEventListener("click", function(event){
+    document.getElementById("caja").setAttribute("style","background-color: #e53e3e");    
+});
+//Cambio de Color 2
+document.getElementById("btn-2").addEventListener("click", function(event){
+    document.getElementById("caja").setAttribute("style","background-color: #dd6b20");    
+});
+//Cambio de Color 3
+document.getElementById("btn-3").addEventListener("click", function(event){
+    document.getElementById("caja").setAttribute("style","background-color: #faf089");    
+});
+//Cambio de Color 4
+document.getElementById("btn-4").addEventListener("click", function(event){
+    document.getElementById("caja").setAttribute("style","background-color: #48bb78");    
+});
+//Cambio de Color 5
+document.getElementById("btn-5").addEventListener("click", function(event){
+    document.getElementById("caja").setAttribute("style","background-color: #81e6d9");    
+});
+//Cambio de Color 6
+document.getElementById("btn-6").addEventListener("click", function(event){
+    document.getElementById("caja").setAttribute("style","background-color: #d53f8c");    
+});
+
+
+
+
+
+
+    
